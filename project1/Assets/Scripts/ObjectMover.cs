09051ebb719537e8aad31b17a10395f456cf7e9a
Kamil Deniz Coşkuner <denizcoskuner807@gmail.com>
2025-02-23 15:15:32 +0300

    adjust ObjectMover to use RigidBody physics

--- project1/Assets/Scripts/ObjectMover.cs
+++ project1/Assets/Scripts/ObjectMover.cs
@@ -4,12 +4,19 @@ public class ObjectMover : MonoBehaviour
 {
     public float speed = 5f;
 
-    void Update()
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    void FixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveX, 0, moveZ) * speed * Time.deltaTime;
-        transform.Translate(movement);
+        Vector3 movement = new Vector3(moveX, 0, moveZ) * speed;
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
     }
 }
